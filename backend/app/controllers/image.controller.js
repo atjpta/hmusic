56b01a6df1cd54c17a15af9789c0feb102c32921
atjpta <caoducan351@gmail.com>
2023-03-12 01:22:49 +0700

    fix

--- backend/app/controllers/image.controller.js
+++ backend/app/controllers/image.controller.js
@@ -1,40 +1,41 @@
 const mongoose = require("mongoose");
-const uri = require("../config").db.uri;
+const config = require("../config");
 
 let bucket;
-mongoose.connection.on("connected", () => {
-  var client = mongoose.connections[0].client;
-  var db = mongoose.connections[0].db;
-  bucket = new mongoose.mongo.GridFSBucket(db, {
+
+mongoose.connect(config.db.uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+  const client = db.client;
+  bucket = new mongoose.mongo.GridFSBucket(db.db, {
     bucketName: "images"
   });
 });
 
-exports.uploadImage =  (req, res) => {
+exports.uploadImage = (req, res) => {
   res.status(200).send("File uploaded successfully");
-}
+};
 
-exports.getImage = async (req, res) => {
-    const file = bucket
+exports.getImage = (req, res) => {
+  const file = bucket
     .find({
       filename: req.params.filename
     })
     .toArray((err, files) => {
-      if (!files || files.length === 0) {
-        return res.status(404)
-          .json({
-            err: "no files exist"
-          });
+      if (err) {
+        console.error(err);
+        return res.status(500).json({
+          err: "Internal server error"
+        });
       }
-      bucket.openDownloadStreamByName(req.params.filename)
-        .pipe(res);
+      if (!files || files.length === 0) {
+        return res.status(404).json({
+          err: "No files exist"
+        });
+      }
+      bucket.openDownloadStreamByName(req.params.filename).pipe(res);
     });
 };
-
-
-
-
-
-
-  
-  
