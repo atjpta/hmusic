eb681ac1822ec7820fb785638229a985ce3065f7
atjpta <caoducan351@gmail.com>
2023-04-01 01:03:35 +0700

    mau` cai logo

--- client/components/other/vHeader.vue
+++ client/components/other/vHeader.vue
@@ -5,11 +5,17 @@
         <div class="flex">
           <OtherVNav @click="open = true" />
           <div @click="navigateTo('/')"
-            class="lg:ml-10 btn btn-ghost bg-transparent hover:bg-transparent text-xl font-bold">
+            class="lg:ml-10 btn btn-ghost bg-transparent hover:bg-transparent font-bold indicator">
             <span
-              class="before:block before:absolute before:-inset-1 before:-skew-x-12 before:rounded-tl-full before:rounded-br-full before:bg-gradient-to-r before:from-green-400 before:via-cyan-400 before:to-blue-400 relative inline-block">
-              <span class="relative text-white px-3">Hmusic</span>
+              class="before:duration-500 before:block before:ring-4 before:ring-green-300/30 before:absolute before:-inset-1 before:-skew-x-12 before:-skew-y-6 before:rounded-tl-full before:rounded-br-full before:bg-gradient-to-r before:from-green-400 before:via-cyan-400 before:to-blue-400 relative inline-block">
+              <span
+                class="before:animate-ping before:w-6 before:h-6 before:rounded-full before:top-1 before:left-9 before:right-0.5 before:duration-500 before:block before:ring-4 before:ring-green-300/30 before:absolute before:-inset-1 before:bg-gradient-to-r before:from-green-400 before:via-cyan-400 before:to-blue-400 relative inline-block">
+                <span class="relative text-white text-lg px-3 z-10">Hmusic</span>
+              </span>
             </span>
+            <div class="indicator-item top-5 bottom-0.5 right-16">
+              <OtherVIcon class-icon="text-red-500 animate-spin text-3xl " icon="fa-solid fa-compact-disc" />
+            </div>
           </div>
         </div>
         <div class="sm:flex hidden">
